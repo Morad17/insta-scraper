e94efe85829ffaec63317cc977d747bdc5588e71
morad17 <morad123@hotmail.co.uk>
2025-08-19 23:52:38 +0100

    creted connection to insta scraper api

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import axios from "axios";
 
 dotenv.config();
 
@@ -8,6 +9,31 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 const port = process.env.PORT || 8000;
+
+app.get("/get-profile", async (req, res) => {
+  const url = "https://instagram120.p.rapidapi.com/api/instagram/profile";
+  const username = req.query.username || "moradelb";
+  const options = {
+    method: "POST",
+    url: url,
+    headers: {
+      "x-rapidapi-key": process.env.RAPID_API_AUTH_KEY,
+      "x-rapidapi-host": process.env.RAPID_API_HOST,
+      "Content-Type": "application/json",
+    },
+    data: {
+      username: username,
+    },
+  };
+  try {
+    const response = await axios(options);
+    console.log(response.data); // Log to server console
+    res.json(response.data); // Send to client
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to fetch profile" });
+  }
+});
 
 // Initialise Node App//
 app.listen(port, () => {
